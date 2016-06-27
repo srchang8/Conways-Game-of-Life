14e5d2fa8b79be32ef4abdcdce02e7442e20c9c4
srchang8 <srchang8@gmail.com>
2016-06-27 11:41:11 -0400

    initial commit

---
+++ Assignment1/InstrumentationViewControllers.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assignment1
+//
+//  Created by stephen chang on 6/21/16.
+//  Copyright © 2016 Stephen Chang. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentationViewControllers: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
