9b3e06989727eb4506b13c84c48384b572e2d659
srchang8 <srchang8@gmail.com>
2016-07-04 14:15:24 -0400

    right before adding the switch statement

--- Assignment2/Assignment2/Problem2ViewController.swift
+++ Assignment2/Assignment2/Problem2ViewController.swift
@@ -21,7 +21,148 @@ class Problem2ViewController: UIViewController {
         
         textViewProblem2.text = "Run Button Pressed"
         
-        //Create 2 dimensional array of type bool - name it "before"
+        
+        
+        //constant variable in case we want to change the size of game of life grid
+        let NumColumns: Int = 10
+        let NumRows: Int = 10
+        
+        var numOfLivingCellsInBefore: Int = 0
+        
+        
+        //create an array of type bool
+        var before = Array(count:NumColumns, repeatedValue: Array(count:NumRows, repeatedValue:false))
+        //create second array to track first array
+        var after = Array(count:NumColumns, repeatedValue: Array(count:NumRows, repeatedValue:false))
+        
+        
+        //check if the cell is dead given the coordinate
+        func checkCoordinates( x: Int, y: Int) -> Bool{
+            
+            var checkedX: Int = x
+            var checkedY: Int = y
+            
+            
+            //check if coordinates are out of bounds
+            // when out of bounds is -1 or = size of array,
+            //it will convert to size -1 to be able to check other side of array
+            if x < 0 {
+                checkedX = NumColumns - 1
+            }
+            
+            if x >= NumColumns {
+                checkedX = NumColumns - 1
+                
+            }
+            
+            if y < 0 {
+                checkedY = NumRows - 1
+            }
+            
+            if y >= NumRows {
+                checkedY = NumRows - 1
+            }
+            
+            //check if cell is alive
+            return before[checkedX][checkedY]
+            
+        }
+        
+        
+        //check the neighbors around the cell to see if alive
+        func checkNeighbors(x: Int, y: Int) -> Bool {
+            
+            
+            var aliveCount: Int = 0
+            var alive: Bool = false
+            
+            //check top left
+            if checkCoordinates( x-1, y: y-1 ) {
+                aliveCount+=1
+            }
+            
+            //check top
+            if checkCoordinates( x, y: y-1 ){
+                aliveCount+=1
+            }
+            
+            //check top right
+            if checkCoordinates( x+1, y: y-1){
+                aliveCount+=1
+            }
+            
+            //check right
+            if checkCoordinates( x+1, y: y){
+                aliveCount+=1
+            }
+            
+            //check bottom right
+            if checkCoordinates( x+1, y: y-1){
+                
+            }
+            
+            //check bottom
+            if checkCoordinates( x, y: y-1){
+                aliveCount+=1
+            }
+            
+            //check bottom left
+            if checkCoordinates( x-1, y: y-1){
+                aliveCount+=1
+            }
+            
+            
+            if aliveCount == 2 || aliveCount == 3 {
+                //return alive
+                alive = true
+            }
+            
+            // return if cell given in the parameter should be alive 
+            //by checking its neighbors and taking in consideration if the coordinates go out of bounds
+            return alive
+            
+            
+        }
+        
+        
+        //set a random true or false value for each cell in the before array
+        for y in 0..<NumColumns{//iterate 0-9
+            for x in 0..<NumRows{//iterate 0-9
+                
+                // if arc4random_uniform generates 1, then it will set cell to true
+                if arc4random_uniform(3) == 1 {
+                    before[y][x] = true
+                    numOfLivingCellsInBefore+=1
+                    
+                }
+                //print(before[y][x]) //print entire array to see if random variables generated correctly
+            }
+            
+        }
+        
+        
+        //count and print number of living cells in before
+        textViewProblem2.text="Number of living cells in before: \(numOfLivingCellsInBefore)"
+        
+        
+        
+        
+        //Check each cell in before and send result to after
+        for y in 0..<NumColumns{//iterate 0-9
+            for x in 0..<NumRows{//iterate 0-9
+                
+                
+                if checkNeighbors( x, y: y) = true{
+                    after[y][x] = true
+                } else if checkNeighbors( x, y: y) = false{
+                    after[y][x] = false
+                }
+                
+                //print(before[y][x]) //print entire array to see if random variables generated correctly
+            }
+            
+        }
+        
         
     }
     
@@ -35,35 +176,11 @@ class Problem2ViewController: UIViewController {
         self.title = "Problem 2"// not sure if this works
         
         
-        //test
         
-        //constant variable in case we want to change the size of game of life grid
-        let NumColumns = 10
-        let NumRows = 10
-        
-        //create an array of type bool
-        var before = Array(count:NumColumns, repeatedValue: Array(count:NumRows, repeatedValue:false))
-        //create second array to track first array
-        var after = Array(count:NumColumns, repeatedValue: Array(count:NumRows, repeatedValue:false))
-        
-        //set a random true or false value for each cell in the before array
-        for y in 0..<NumColumns{//iterate 0-9
-            for x in 0..<NumRows{//iterate 0-9
-                
-                // if arc4random_uniform generates 1, then it will set cell to true
-                if arc4random_uniform(3) == 1 {
-                    before[y][x] = true
-                    
-                }
-                print(before[y][x]) //print entire array to see if random variables generated correctly
-            }
-            
-        }
+       
         
         
         
-        
-        //test end
         
         
     }
