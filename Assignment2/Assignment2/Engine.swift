870e68d7a589f139f290fe12734484d71c322edb
srchang8 <srchang8@gmail.com>
2016-07-05 15:50:57 -0400

    implemented step2()

--- Assignment2/Assignment2/Engine.swift
+++ Assignment2/Assignment2/Engine.swift
@@ -210,57 +210,46 @@ func neighbors(x: Int, y: Int) -> Array<(xCoord: Int, yCoord: Int)>{
 
 func step2(twoDimArrOfBools: Array<Array<Bool>> ) -> Array<Array<Bool>>{
     
-    var NumColumns = twoDimArrOfBools.count
-    var NumRows = twoDimArrOfBools.count
     var numOfLivingCellsInBefore: Int = 0
     var numOfLivingCellsInAfter: Int = 0
+    var livingNeighborCells: Int = 0
     
+    
+    //array to hold the updated value for cells
     var after = Array(count:twoDimArrOfBools.count, repeatedValue: Array(count:twoDimArrOfBools.count, repeatedValue:false))
     
-    
-    
-    //check if the cell is dead given the coordinate
-    func checkCoordinates( x: Int, y: Int) -> Bool{
-        
-        var checkedX: Int = x
-        var checkedY: Int = y
-        
-        
-        //check if coordinates are out of bounds
-        // when out of bounds is -1 or = size of array,
-        //it will convert to size -1 to be able to check other side of array
-        if x < 0 {
-            checkedX = NumColumns - 1
-        }
-        
-        if x >= NumColumns {
-            checkedX = NumColumns - 1
+    for i in 0..<twoDimArrOfBools.count{
+        for j in 0..<twoDimArrOfBools.count{//i and j traverse through 10x10
             
-        }
-        
-        if y < 0 {
-            checkedY = NumRows - 1
-        }
-        
-        if y >= NumRows {
-            checkedY = NumRows - 1
-        }
-        
-        //check if cell is alive
-        return twoDimArrOfBools[checkedX][checkedY]
-        
-    }
-    
-    //Check each cell in before and send result to after
-    for y in 0..<NumColumns{
-        for x in 0..<NumRows{
-            
-           
-            
-        }
-        
-    }
+            for k in 0..<twoDimArrOfBools.count{// k traverses through all the neighbors that are stored in the array of tuples
 
+                //check the list of neighbors from twoDimArrOfBools[i][j]
+                if twoDimArrOfBools[neighbors(i, y: j)[i].xCoord][neighbors(i, y: j)[i].yCoord] == true{
+                    livingNeighborCells+=1
+                    
+                }
+                
+            }
+            
+            switch twoDimArrOfBools[i][j]{
+            case true:
+                if livingNeighborCells == 3 || livingNeighborCells == 2{
+                    after[i][j] = true
+                    numOfLivingCellsInAfter += 1
+                    livingNeighborCells = 0
+                }
+            case false:
+                if livingNeighborCells == 3{
+                    after[i][j] = true
+                    numOfLivingCellsInAfter += 1
+                }
+            }
+
+        }
+        
+    }
+    
+   
     
     
     
