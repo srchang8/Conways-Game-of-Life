30a566314e5dacc63af740ffb5196dc4164a824a
srchang8 <srchang8@gmail.com>
2016-07-11 11:35:26 -0400

    save checking

--- Assignment3/Assignment3/GridView.swift
+++ Assignment3/Assignment3/GridView.swift
@@ -33,24 +33,66 @@ class GridView: UIView {
 
     override func drawRect(rect: CGRect){
         
-        let context = UIGraphicsGetCurrentContext()
+        let context = UIGraphicsGetCurrentContext()//the object is the brush
         CGContextSetLineWidth(context, gridWidth)
         CGContextSetStrokeColorWithColor(context, gridColor)
         
-        //Straight line
-        CGContextMoveToPoint(context, 30, 30)
-        CGContextAddLineToPoint(context, 550, 30)
+       
+        var xCoord: CGFloat = 550
+        var yCoord: CGFloat = 30
         
-        //Actually draw the path
-        CGContextStrokePath(context)
+        
+        for var y = 0; y < rows; y+=1 {
+            
+            
+            
+            //Straight line
+            CGContextMoveToPoint(context, 30, 30)//start here
+            CGContextAddLineToPoint(context, xCoord, yCoord)//end here
+            
+            //length of each horizontal line is 520
+            xCoord = xCoord + 520
+            
+            //Distance from start point to next start point
+            yCoord = yCoord + 10
+            
+            
+        
+            //Actually draw the path
+            CGContextStrokePath(context)
+            
+        }
     }
    
-    
+
+    //function that takes in coordinates and draws a line from that
+    func drawRowLine(xCoord: CGFloat, yCoord: CGFloat){
         
-    
+        let brushContext = UIGraphicsGetCurrentContext()
+        CGContextSetLineWidth(brushContext, gridWidth)
+        CGContextSetStrokeColorWithColor(brushContext, gridColor)
+        
+        let lengthOfLine: Int = 500
+        let heightBetweenLines: Int = 30
+        
+        CGContextMoveToPoint(brushContext, xCoord, yCoord)//start here
+        
+        
+        
+        CGContextAddLineToPoint(brushContext, xCoord, yCoord)//end here
+        
+        
+        //Actually draw the path
+        CGContextStrokePath(brushContext)
+        
+        
+        
+    }
     
 }
+//function that takes in coordinates and returns a coordinate for CGContextAddLineToPoint
 
+//
 
 
 
